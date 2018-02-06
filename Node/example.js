0bced31454aa3fb7717a8dd34853a884b851dab1
xdagency <pandabear00001@outlook.com>
2018-02-05 21:26:27 -0500

    Moved code into different file

--- Node/example.js
+++ Node/example.js
@@ -3,20 +3,3 @@
 */
 
 console.log('Hello World');
-
-
-/*
-  Learning about loops and conditional statements
-*/
-
-for (var i = 1; i < 101; i++) {
-	if (i % 5 == 0 && i % 3 != 0) {
-		  console.log('fizz');
-    } else if (i % 3 == 0 && i % 5 != 0) {
-		    console.log('buzz');
-    } else if (i % 15 == 0) {
-		    console.log('fizzbuzz');
-    } else {
-		    console.log(i);
-    }
-}
--- myfile.js
+++ myfile.js
@@ -1,6 +1,26 @@
+/*
+  Learning about vars and arrays
+*/
+
 var wrapper = getElementById("wrapper");
 var fruits = ["apple", "pear", "orange"];
 
 if (fruits[1] == "pear") {
   alert("There's a pear here.")
+}
+
+/*
+  Learning about loops and conditional statements
+*/
+
+for (var i = 1; i < 101; i++) {
+	if (i % 5 == 0 && i % 3 != 0) {
+		  console.log('fizz');
+    } else if (i % 3 == 0 && i % 5 != 0) {
+		    console.log('buzz');
+    } else if (i % 15 == 0) {
+		    console.log('fizzbuzz');
+    } else {
+		    console.log(i);
+    }
 }
